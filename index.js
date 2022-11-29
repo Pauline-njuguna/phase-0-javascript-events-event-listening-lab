4d1bc134f22c8f89d6d2459061cea1139954992d
pauline-njunguna <paulinenjunguna.w@gmail.com>
2022-11-29 22:41:03 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+const input = document.getElementById("input");
+
+function clickAlert(){
+    alert('Iwas clicked!')
+}
+input.addEventListener('click', clickAlert())
 }
